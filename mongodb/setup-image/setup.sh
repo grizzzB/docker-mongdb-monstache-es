a43badd4a206a2eaa4b2efeee37397f10d5dd986
grizzzB <kago1230@gmail.com>
2022-01-04 04:30:34 +0900

    mongodb replica setup 도커 빌드 compose에서 수행하도록 변경

--- mongodb/setup-image/setup.sh
+++ mongodb/setup-image/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sleep 10 | echo Sleeping
+echo "Waiting for MongoDB startup.."
+until [ "$(mongo mongodb://mongo1:27017 --eval "printjson(db.serverStatus())" | grep uptime | head -1)" ]; do
+  printf '.'
+  sleep 1
+done
+
+echo $(mongo mongodb://mongo1:27017 admin --eval "db.auth(\"datadog\", \"datadog\");printjson(db.serverStatus())" | grep uptime | head -1)
+echo "MongoDB Started.."
 
 mongo mongodb://mongo1:27017 replicaSet.js
